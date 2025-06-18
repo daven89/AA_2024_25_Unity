7c929b6f26f673df27bc3b4abde3daaf5668d252
P0stMelone <riccardo.melone@outlook.com>
2025-06-18 18:24:13 +0200

    NPC Esercizio

---
+++ Unity_Introduction/Assets/Scripts/NPCComponent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCComponent : MonoBehaviour
+{
+    [SerializeField]
+    private bool canInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
